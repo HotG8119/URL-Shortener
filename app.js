0e7e5842ea574ff58d8b56df15488cdad2256410
HotG8119 <k508060@gmail.com>
2023-05-18 22:49:05 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -7,21 +7,7 @@ const routes = require("./routes");
 const port = 3000;
 const domain = `http://localhost:${port}`;
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
+require("./config/mongoose");
 
 const app = express();
 // setting template engine
